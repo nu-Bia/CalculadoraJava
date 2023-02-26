1dc0f4078029f91a9098c486d957222714e03f71
Núbia <112019351+nu-Bia@users.noreply.github.com>
2023-02-26 18:08:19 -0300

    Add files via upload

---
+++ Calculadora/app/src/main/java/com/calculadora/Expression.java
@@ -0,0 +1,7 @@
+package com.calculadora;
+
+public class Expression {
+    public double evaluate() {
+        return 0;
+    }
+}
---
+++ Calculadora/app/src/main/java/com/calculadora/MainActivity.java
@@ -0,0 +1,198 @@
+package com.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private android.widget.Button numeroZero,numeroUm,numeroDois,numeroTres,numeroQuatro,numeroCinco,numeroSeis,numeroSete,numeroOito,
+            numeroNove,ponto,soma,subtracao,multiplicacao,divisao,igual,botao_limpar;
+
+    private android.widget.TextView txtExpressao,txtResultado;
+    private android.widget.ImageView backspace;
+
+    public MainActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IniciarComponentes();
+        getSupportActionBar().hide();
+
+        numeroZero.setOnClickListener(this);
+        numeroUm.setOnClickListener(this);
+        numeroDois.setOnClickListener(this);
+        numeroTres.setOnClickListener(this);
+        numeroQuatro.setOnClickListener(this);
+        numeroCinco.setOnClickListener(this);
+        numeroSeis.setOnClickListener(this);
+        numeroSete.setOnClickListener(this);
+        numeroOito.setOnClickListener(this);
+        numeroNove.setOnClickListener(this);
+        ponto.setOnClickListener(this);
+        soma.setOnClickListener(this);
+        multiplicacao.setOnClickListener(this);
+        divisao.setOnClickListener(this);
+
+        botao_limpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtExpressao.setText("");
+                txtResultado.setText("");
+            }
+        });
+
+        backspace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView expressao = findViewById(R.id.txt_expressao);
+                String string = expressao.getText().toString();
+
+                if (!string.isEmpty()){
+                    byte var0 = 0;
+                    int var1 = string.length()-1;
+                    String txt_expressao = string.substring(var0,var1);
+                    expressao.setText(txt_expressao);
+                }
+                txtResultado.setText("");
+
+                }
+
+        });
+             igual.setOnClickListener(new View.OnClickListener () {
+                 @Override
+
+                 public void onClick (View v) {
+
+                 try {
+               Expression expressao ;
+
+                     expressao = new ExpressionBuild (txtExpressao.getText().toString()).build();
+                     double resultado = expressao.evaluate();
+                 long longResult = (long) resultado;
+
+                 if(resultado == (double)longResult){
+                     txtResultado.setText((CharSequence) String.valueOf(longResult));
+                 }else {
+                     txtResultado.setText((CharSequence)String.valueOf(resultado));
+                 }
+
+             } catch (Exception e) {
+                 }}
+
+
+    });
+    }
+    private void IniciarComponentes(){
+        numeroZero = findViewById(R.id.numero_zero);
+        numeroUm = findViewById(R.id.numero_um);
+        numeroDois = findViewById(R.id.numero_dois);
+        numeroTres = findViewById(R.id.numero_tres);
+        numeroQuatro= findViewById(R.id.numero_quatro);
+        numeroCinco = findViewById(R.id.numero_cinco);
+        numeroSeis = findViewById(R.id.numero_seis);
+        numeroSete = findViewById(R.id.numero_sete);
+        numeroOito= findViewById(R.id.numero_oito);
+        numeroNove = findViewById(R.id.numero_nove);
+        ponto = findViewById(R.id.ponto);
+        soma  = findViewById(R.id.soma);
+        subtracao = findViewById(R.id.subtracao);
+        multiplicacao = findViewById(R.id.multiplicacao);
+        divisao = findViewById(R.id.bt_divisao);
+        igual = findViewById(R.id.igual);
+        botao_limpar = findViewById(R.id.bt_limpar);
+        txtExpressao = findViewById(R.id.txt_expressao);
+        txtResultado = findViewById(R.id.txt_resultado);
+        backspace = findViewById(R.id.backspace);
+    }
+
+    public void AcrescentarUmaExpressao(String string,boolean botao_limpar){
+        if (txtResultado.getText().equals("")){
+            txtExpressao.setText("");
+        }
+        if (botao_limpar){
+            txtResultado.setText(" ");
+            txtExpressao.append(string);
+        }else{
+            txtExpressao.append(txtResultado.getText());
+            txtExpressao.append(string);
+            txtResultado.setText(" ");
+        }
+    }
+
+    @Override
+    public void onClick(View view ) {
+        switch (view.getId()){
+            case R.id.numero_zero:
+                AcrescentarUmaExpressao("0", true);
+                break;}
+
+                switch (view.getId()){
+                    case R.id.numero_um:
+                    AcrescentarUmaExpressao("1", true);
+                    break;}
+                switch (view.getId()){
+                    case R.id.numero_dois:
+                        AcrescentarUmaExpressao("2", true);
+                        break;}
+                    switch (view.getId()){
+                        case R.id.numero_tres:
+                            AcrescentarUmaExpressao("3", true);
+                            break;}
+                        switch (view.getId()){
+                            case R.id.numero_quatro:
+                                AcrescentarUmaExpressao("4", true);
+                                break; }
+                        switch (view.getId()){
+                                case R.id.numero_cinco:
+                                    AcrescentarUmaExpressao("5", true);
+                                    break;}
+                                switch (view.getId()){
+                                    case R.id.numero_seis:
+                                        AcrescentarUmaExpressao("6", true);
+                                        break;}
+                                    switch (view.getId()){
+                                        case R.id.numero_sete:
+                                            AcrescentarUmaExpressao("7", true);
+                                            break;}
+                                        switch (view.getId()){
+                                            case R.id.numero_oito:
+                                                AcrescentarUmaExpressao("8", true);
+                                                break;}
+                                            switch (view.getId()){
+                                                case R.id.numero_nove:
+                                                    AcrescentarUmaExpressao("0", true);
+                                                    break;}
+                                                    switch (view.getId()){
+                                                        case R.id.ponto:
+                                                        AcrescentarUmaExpressao(".", true);
+                                                        break;}
+
+                                                        switch (view.getId()){
+                                                            case R.id.soma:
+                                                                AcrescentarUmaExpressao("+", false);
+                                                                break;}
+                                                            switch (view.getId()){
+                                                                case R.id.soma:
+                                                                    AcrescentarUmaExpressao("+", false);
+                                                                    break;}
+                                                                switch (view.getId()){
+                                                                    case R.id.subtracao:
+                                                                        AcrescentarUmaExpressao("-", false);
+                                                                        break;}
+                                                                    switch (view.getId()){
+                                                                        case R.id.multiplicacao:
+                                                                            AcrescentarUmaExpressao("*", false);
+                                                                            break;}
+                                                                        switch (view.getId()){
+                                                                            case R.id.bt_divisao:
+                                                                                AcrescentarUmaExpressao("/", false);
+                                                                                break;}
+        }
+    }
